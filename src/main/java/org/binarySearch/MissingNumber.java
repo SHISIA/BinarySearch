73d2eedd1bc7bbd60e1bb0d5ef6232d0ae0a4f40
Shisia <jamesshisia6@gmail.com>
2023-07-02 16:51:01 +0300

    LeetCode MissingNumber

--- src/main/java/org/binarySearch/MissingNumber.java
+++ src/main/java/org/binarySearch/MissingNumber.java
@@ -1,12 +1,19 @@
 package org.binarySearch;
 
 public class MissingNumber {
-    public int missingNumber(int[] nums) {
+    public static int missingNumber(int[] nums) {
+//        https://leetcode.com/problems/missing-number
         //arithmetic progression formula
         int n = nums.length*(nums.length + 1)/2;
+        int sum=0;
+        System.out.println("n "+n);
         for(int i=0;i<nums.length;i++){
-            n-=i;
+            sum+=nums[i];
         }
-        return n;
+        return n-sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(missingNumber(new int[]{9,6,4,2,3,5,7,0,1}));
     }
 }
